0c14330c5136a5caf39fe30aee3d001dd86c8445
GeorgeCast <georgecast12@gmail.com>
2019-04-10 00:02:45 -0700

    working game - pending revision

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,10 +2,7 @@
 //Variables
 
 // Holds the number  for 
-var totalScore = [];
-var scoreCounter = '';
 var userCounter = 0;
-var pcNumber = "";
 var wins = 0;
 var losses = 0;
 
@@ -15,61 +12,120 @@ var losses = 0;
 $(function () {
 
     // random number ()
-    var pcNumber = Math.floor((Math.random() * 121) + 19);
+    var pcNumber = Math.floor((Math.random() * 120) + 19);
     $("#pcGuess").html(pcNumber);
 
 
-    // // random values between (1-12) are assigned to each button 
-    // var ruby = Math.floor((Math.random() * 13) + 1);
-    // var emerald = Math.floor((Math.random() * 13) + 1);
-    // var sapphire = Math.floor((Math.random() * 13) + 1);
-    // var canary = Math.floor((Math.random() * 13) + 1);
+    // random values between (1-12) are assigned to each button 
+    var ruby = Math.floor((Math.random() * 13) + 1);
+    var emerald = Math.floor((Math.random() * 13) + 1);
+    var sapphire = Math.floor((Math.random() * 13) + 1);
+    var canary = Math.floor((Math.random() * 13) + 1);
 
 
-    // console.log(ruby)
-    // console.log(emerald)
-    // console.log(sapphire)
-    // console.log(canary)
-    // for (var i = 0; i < userOptions.length; i++) {
+    console.log(ruby)
+    console.log(emerald)
+    console.log(sapphire)
+    console.log(canary)
 
-    // // random values between (1-12) are assigned to each button 
+    // //user will click over the buttons and they will be added in the userGuess field
 
+    $("#r").on('click', function () {
+        userCounter = userCounter + ruby;
 
+        $("#userGuess").html(userCounter);
 
-
-    //user will click over the buttons and they will be added in the userGuess field
-
-    $(".crystalimg").on('click', function () {
-        var gemValue = Math.floor((Math.random() * 13) + 1);
-        var scoreCounter = ($(this).attr(gemValue));
-
-        $("#userGuess").html(scoreCounter);
-
-        console.log(scoreCounter)
-
-
-        //if the user matches the PC generated number it will add a win & reset values for PC and gems
-        // if the user goes above the the PC generated number it will add a lose & reset values for PC and gems
-
-
-        if (userCounter === targetNumber) {
-            alert("You win!");
+        if (userCounter === pcNumber) {
             wins++;
-            var pcNumber = Math.floor((Math.random() * 121) + 19);
-            $("#pcGuess").html(pcNumber);
+            $("#w").html("Wins: " + wins);
+            location.reload(pcNumber);
+
+            console.log(userCounter)
+            console.log(wins)
 
         }
 
-        else if (userCounter >= targetNumber) {
-            alert("You lose!!");
+        else if (userCounter >= pcNumber) {
             losses++;
-            var pcNumber = Math.floor((Math.random() * 121) + 19);
-            $("#pcGuess").html(pcNumber);
+            $("#l").html("losses: " + losses);
+            location.reload(pcNumber);
+
         }
-
-
-
     })
 
+    $("#e").on('click', function () {
+        userCounter = userCounter + emerald;
 
-});
+        $("#userGuess").html(userCounter);
+
+        if (userCounter === pcNumber) {
+            wins++;
+            $("#w").html("Wins: " + wins);
+            location.reload(pcNumber);
+
+            console.log(userCounter)
+            console.log(wins)
+
+        }
+
+        else if (userCounter >= pcNumber) {
+            losses++;
+            $("#l").html("losses: " + losses);
+            location.reload(pcNumber);
+        }
+
+    })
+    $("#s").on('click', function () {
+        userCounter = userCounter + sapphire;
+
+        $("#userGuess").html(userCounter);
+
+        if (userCounter === pcNumber) {
+            wins++;
+            $("#w").html("Wins: " + wins);
+            location.reload(pcNumber);
+
+            console.log(userCounter)
+            console.log(wins)
+
+        }
+
+        else if (userCounter >= pcNumber) {
+            losses++;
+            $("#l").html("losses: " + losses);
+            location.reload(pcNumber);
+        }
+    })
+    $("#c").on('click', function () {
+        userCounter = userCounter + canary;
+
+        $("#userGuess").html(userCounter);
+
+        if (userCounter === pcNumber) {
+            wins++;
+            $("#w").html("Wins: " + wins);
+            location.reload(pcNumber);
+
+            console.log(userCounter)
+            console.log(wins)
+
+        }
+
+        else if (userCounter >= pcNumber) {
+            losses++;
+            $("#l").html("losses: " + losses);
+            location.reload(pcNumber);
+        }
+    })
+
+})
+//if the user matches the PC generated number it will add a win & reset values for PC and gems
+// if the user goes above the the PC generated number it will add a lose & reset values for PC and gems
+
+
+
+
+
+
+
+
